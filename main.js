8e4af1b95222fe5e7a6a980b6330e2fc8bd0d6a0
Anon1166 <anon.2222@mail.kmutt.ac.th>
2022-09-14 20:32:37 +0700

    V6

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 const { template } = require('@babel/core')
 function minMedMax(n1, n2, n3) {
     let array = [n1,n2,n3];
-    let swap,done = false,
-        swapped;
-    while (!done){
-      swapped=0;
-      for(i=1;i<array.length;i++){
+    let swap;
+
+    for(k=1; k<array.length;k++){
+      for(i=1; i<array.length; i++){
         if(array[i-1] > array[i]){
-          swap = array[i];
-          array[i] = array[i-1];
-          array[i-1] = swap
-          swapped = 1;
-        }
-        if(swapped == 0){
-          done = true
+          swap = array[i]
+          array[i] = array[i-1]
+          arra[i-1] = swap
         }
       }
     }
-    return {min: array[0], mid: array[1] ,max: array[2]};// v
+    return {min: array[0], mid: array[1] ,max: array[2]};
 }
 module.exports = minMedMax
